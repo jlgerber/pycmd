e69b9ad6003c94fbc4fc23a53aade8b87e185317
jonathan gerber <jlgerber@gmail.com>
2019-02-25 06:00:24 -0800

    adds impl

--- pycmd/__init__.py
+++ pycmd/__init__.py
@@ -1,4 +1,4 @@
-rom abc import ABCMeta
+from abc import ABCMeta
 from abc import abstractmethod
 import logging
 
@@ -19,12 +19,65 @@ class Command(object):
 class Executor(object):
     def __init__(self, logger=logging.getLogger(__name__)):
         self._stack = []
+        self._cmd_stack = []
         self._exceptions = []
         self.logger = logger
+        self.failed = False
+
+    def add_cmd(self, cmd):
+        """
+        Add a command
+        """
+        assert isinstance(cmd, Command), "cmd must be instance of pycmd.Command"
+        self._cmd_stack.append(cmd)
+
+    def execute(self):
+        """
+        Execute commands which have previously been added
+        """
+        def any_fail(a,b):
+            """if either a or b fail, return False"""
+            if a == False or b == False:
+                return False
+            return True
+
+        if len(self._cmd_stack) == 0:
+            raise RuntimeError("Command Stack is Empty")
+
+        for cmd in self._cmd_stack:
+            if not self._exec_cmd(cmd):
+                return False
+        return True
+
+    def _exec_cmd(self, cmd):
+        try:
+            self.logger.debug("executor.exec_cmd calling command: {}".format(cmd))
+            cmd.do()
+            self._stack.append(cmd)
+            return True
+        except Exception:
+            self.failed = True
+            try:
+                self.logger.error("exeutor.exec_cmd() failed. Unwinding stack.")
+                self.logger.debug("calling cmd.undo()")
+                cmd.undo()
+            except Exception as err:
+                self.logger.error("executor.exec_cmd() undo failed with '{}'".format(err))
+                self._exceptions.append(err)
+            self.logger.debug("stack len {}".format(len(self._stack)))
+            while len(self._stack) > 0:
+                cmd = self._stack.pop()
+                self.logger.debug("calling cmd.undo on '{}'".format(cmd))
+                try:
+                    cmd.undo()
+                except Exception as err:
+                    self.logger.error("executor.exec_cmd() unwinding stack failure: '{}'".format(err))
+                    self._exceptions.append(err)
+            return False
 
     def exec_cmd(self, cmd):
         """
-        execute command. If successful, add
+        Execute command. If successful, add
         it to the stack. Otherwise, unwind the
         stack
 
@@ -34,27 +87,12 @@ class Executor(object):
         :return: success or failure
         :return type: bool
         """
-        try:
-            self.logger.debug("executor.exec_cmd calling command: {}".format(cmd))
-            cmd.do()
-            return True
-        except Exception:
-            try:
-                self.logger.error("exeutor.exec_cmd() failed. Unwinding stack.")
-                self.logger.debug("calling cmd.undo()")
-                cmd.undo()
-            except Exception as err:
-                self.logger.error("executor.exec_cmd() undo failed with '{}'".format(err))
-                self._exceptions.append(err)
-            while self._stack.len():
-                cmd = self._stack.pop()
-                self.debug("calling cmd.undo on '{}'".format(cmd))
-                try:
-                    cmd.undo()
-                except Exception as err:
-                    self.logger.error("executor.exec_cmd() unwinding stack failure: '{}'".format(err))
-                    self._exceptions.append(err)
+        assert len(self._cmd_stack) == 0, "cannot mix exe_cmd and add_cmd/exe"
+
+        if self.failed:
+            self.logger.error("executor.exec_cmd short circuting")
             return False
+        return self._exec_cmd(cmd)
 
     @property
     def exceptions(self):
---
+++ tests/test_cmd.py
@@ -0,0 +1,136 @@
+import unittest
+from context import pycmd
+
+class State(object):
+    def __init__(self):
+        self.cmda_exe=None
+        self.cmdb_exe=None
+
+class CmdA(pycmd.Command):
+    def __init__(self, state):
+        self.state = state
+
+    def do(self):
+        self.state.cmda_exe="cmda success"
+
+    def undo(self):
+        self.state.cmda_exe="cmda undone"
+
+
+class CmdB(pycmd.Command):
+    def __init__(self, state):
+        self.state = state
+
+    def do(self):
+        self.state.cmdb_exe="cmdb success"
+
+    def undo(self):
+        self.state.cmdb_exe="cmdb undone"
+
+
+class CmdAFail(pycmd.Command):
+    def __init__(self, state):
+        self.state = state
+
+    def do(self):
+        raise RuntimeError("cmda failed")
+
+    def undo(self):
+        self.state.cmda_exe="cmda undone"
+
+
+class CmdBFail(pycmd.Command):
+    def __init__(self, state):
+        self.state = state
+
+    def do(self):
+        raise RuntimeError("cmdb failed")
+
+    def undo(self):
+        self.state.cmdb_exe="cmdb undone"
+
+
+class ExeCmdTest(unittest.TestCase):
+    def test_cmd_execute_success(self):
+        state = State()
+        cmda = CmdA(state)
+        cmdb = CmdB(state)
+        executor = pycmd.Executor()
+
+        result = executor.exec_cmd(cmda)
+        self.assertTrue(result)
+        result = executor.exec_cmd(cmdb)
+        self.assertTrue(result)
+
+        self.assertEqual(state.cmda_exe, "cmda success")
+        self.assertEqual(state.cmdb_exe, "cmdb success")
+
+    def test_cmd_execute_fail_b(self):
+        state = State()
+        cmda = CmdA(state)
+        cmdb = CmdBFail(state)
+        executor = pycmd.Executor()
+
+        result = executor.exec_cmd(cmda)
+        self.assertTrue(result)
+        result = executor.exec_cmd(cmdb)
+        self.assertFalse(result)
+
+        self.assertEqual(state.cmda_exe, "cmda undone")
+        self.assertEqual(state.cmdb_exe, "cmdb undone")
+
+    def test_cmd_execute_fail_a(self):
+        state = State()
+        cmda = CmdAFail(state)
+        cmdb = CmdB(state)
+        executor = pycmd.Executor()
+
+        result = executor.exec_cmd(cmda)
+        self.assertFalse(result)
+
+        result = executor.exec_cmd(cmdb)
+        self.assertFalse(result)
+
+        self.assertEqual(state.cmda_exe, "cmda undone")
+        self.assertEqual(state.cmdb_exe, None)
+
+
+class AddCmdTest(unittest.TestCase):
+    def test_cmd_execute_success(self):
+        state = State()
+        cmda = CmdA(state)
+        cmdb = CmdB(state)
+        executor = pycmd.Executor()
+
+        executor.add_cmd(cmda)
+        executor.add_cmd(cmdb)
+        executor.execute()
+
+        self.assertEqual(state.cmda_exe, "cmda success")
+        self.assertEqual(state.cmdb_exe, "cmdb success")
+
+    def test_cmd_execute_fail_b(self):
+        state = State()
+        cmda = CmdA(state)
+        cmdb = CmdBFail(state)
+        executor = pycmd.Executor()
+
+        executor.add_cmd(cmda)
+        executor.add_cmd(cmdb)
+        executor.execute()
+
+        self.assertEqual(state.cmda_exe, "cmda undone")
+        self.assertEqual(state.cmdb_exe, "cmdb undone")
+
+    def test_cmd_execute_fail_a(self):
+        state = State()
+        cmda = CmdAFail(state)
+        cmdb = CmdB(state)
+        executor = pycmd.Executor()
+
+        executor.add_cmd(cmda)
+        executor.add_cmd(cmdb)
+        executor.execute()
+
+        self.assertEqual(state.cmda_exe, "cmda undone")
+        self.assertEqual(state.cmdb_exe, None)
